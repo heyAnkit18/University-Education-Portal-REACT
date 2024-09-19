c0c61f3cdb8cd9172dbaf84a119b04d49ae892ca
heyAnkit18 <ankitkumarjune18@gmail.com>
2024-09-19 21:23:03 +0530

    Finishing...

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import Navbar from './components/navbar/Navbar';
+import Navbar from './components/Navbar/Navbar';
 
 import Hero from './components/Hero/Hero'
 import Programs from './components/Programs/Programs'
