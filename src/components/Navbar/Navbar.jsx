332442da93e86197e29c9db9d685d2efdadf572f
heyAnkit18 <ankitkumarjune18@gmail.com>
2024-09-19 20:53:30 +0530

    Removing deploy error again

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,51 +1,41 @@
-import React, { useState, useEffect } from 'react';
-import './Navbar.css';
-import logo from '../../assets/logo.png';
-import menu_icon from '../../assets/menu-icon.png';
+import React, { useState, useEffect } from 'react'
+import './Navbar.css'
+import logo from '../../assets/logo.png'
+import menu_icon from '../../assets/menu-icon.png'
 import { Link } from 'react-scroll';
 
+
+
 const Navbar = () => {
+
   const [sticky, setSticky] = useState(false);
-  const [mobileMenu, setMobileMenu] = useState(false);
-
   useEffect(() => {
-    const handleScroll = () => {
-      setSticky(window.scrollY > 50);
-    };
+    window.addEventListener('scroll', () => {
+      window.scrollY > 50 ? setSticky(true) : setSticky(false);
+    })
+  }, [])
 
-    window.addEventListener('scroll', handleScroll);
-
-    // Cleanup function
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
-
+  const [mobileMenu, setMobileMenu] = useState(false);
   const toggleMenu = () => {
-    setMobileMenu(prevState => !prevState);
-  };
+    mobileMenu ? setMobileMenu(false) : setMobileMenu(true);
+  }
+
 
   return (
-    <nav className={`navbar ${sticky ? 'dark-nav' : ''}`}>
-      <img src={logo} alt="Logo" className='logo' />
-      <ul className={`nav-links ${mobileMenu ? 'show-menu' : 'hide-menu'}`}>
+    <nav className={`container ${sticky ? 'dark-nav' : ''}`}>
+      <img src={logo} alt="" className='logo' />
+      <ul className={mobileMenu ? '':'hide-mobile-menu'}>
         <li><Link to='hero' smooth={true} offset={0} duration={500}>Home</Link></li>
         <li><Link to='program' smooth={true} offset={-260} duration={500}>Program</Link></li>
         <li><Link to='about' smooth={true} offset={-110} duration={500}>About</Link></li>
         <li><Link to='campus' smooth={true} offset={-175} duration={500}>Campus</Link></li>
         <li><Link to='testimonials' smooth={true} offset={-240} duration={500}>Testimonials</Link></li>
-        <li><Link to='contact' smooth={true} offset={-260} duration={500} className='btn'>Contact Us</Link></li>
+        <li><Link to='contact' smooth={true} offset={-260} duration={500} className='btn'>Contact us</Link></li>
       </ul>
-      <img
-        src={menu_icon}
-        alt="Menu Icon"
-        className='menu-icon'
-        onClick={toggleMenu}
-        aria-label="Toggle menu"
-      />
+      <img src={menu_icon} alt="" className='menu-icon' onClick={toggleMenu} />
+
     </nav>
-  );
-};
+  )
+}
 
-export default Navbar;
-
+export default Navbar
