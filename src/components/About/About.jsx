b74c30acb735a32ad3afcd8fbb1ae96a2172f6cb
heyAnkit18 <ankitkumarjune18@gmail.com>
2024-09-19 19:19:33 +0530

    Finishing About

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,26 +1,26 @@
-import React from 'react';
+import React from 'react'
 import './About.css'
-import about_img from '../../assets/about.png';
-import play_icon from '../../assets/play-icon.png';
+import about_img from '../../assets/about.png'
+import play_icon from '../../assets/play-icon.png'
 
-const About = () => {
+const About = ({setPlayVideo}) => {
   return (
     <div className='about'>
-      <div className='about-left'>
-        <img src={about_img} alt="About University" className='about-img' />
-        <img src={play_icon} alt="Play Icon" className='play-icon' />
-      </div>
-      <div className='about-right'>
+        <div className='about-left'>
+            <img src={about_img} alt="" className='about-img' />
+            <img src={play_icon} alt="" className='play-icon' onClick={()=>{setPlayVideo(true)}} />
+        </div>
+        <div className='about-right'>
         <h3>About university</h3>
         <h2>Empowering Minds, Shaping Futures: Vedanta Engineering University</h2>
         <p>At Vedanta Engineering University, we are committed to fostering a culture of academic excellence, innovation, and inclusivity. Our rich history of producing world-class professionals is a testament to our dedication.</p>
         <p>We are dedicated to fostering a culture of academic excellence, innovation, and inclusivity. With a rich history of producing world-class professionals.</p>
         <p>We lead the way in groundbreaking research and technological advancements, constantly pushing the boundaries of knowledge. Our faculty and students collaborate to tackle the challenges of tomorrow.</p>
-        <p>Our faculty and students work together to solve the challenges of tomorrow. We invite you to become part of a university that doesn't just prepare you for the future but empowers you to shape it.</p>
-        <p>Join us at Vedanta Engineering University—where we don’t just prepare you for the future, but empower you to shape it.</p>
-      </div>
+        <p>our faculty and students work together to solve the challenges of tomorrow. We invite you to become part of a university that doesn't just prepare you for the future but empowers you to shape it.</p>
+        <p>Join us at [University Name]—where we don’t just prepare you for the future, but empower you to shape it.</p>
     </div>
-  );
+    </div>
+  )
 }
 
-export default About;
+export default About
