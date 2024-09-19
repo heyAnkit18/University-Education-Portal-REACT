ebf7f08967a41d51b582f97788422f288bbbb93c
heyAnkit18 <ankitkumarjune18@gmail.com>
2024-09-19 20:29:44 +0530

    Removing deploy error

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import React, { useState } from 'react';
-import Navbar from './Components/Navbar/Navbar';
-import Hero from './Components/Hero/Hero';
-import Programs from './Components/Programs/Programs';
-import Title from './Components/Title/Title';
-import About from './Components/About/About';
-import Campus from './Components/Campus/Campus';
-import Testimonials from './Components/Testimonials/Testimonials';
-import Contact from './Components/Contact/Contact';
-import Footer from './Components/Footer/Footer';
-import VideoPlayer from './Components/VideoPlayer/VideoPlayer';
+import Navbar from './Components/Navbar/Navbar'
+import Hero from './Components/Hero/Hero'
+import Programs from './Components/Programs/Programs'
+import Title from './Components/Title/Title'
+import About from './Components/About/About'
+import Campus from './Components/Campus/Campus'
+import Testimonials from './Components/Testimonials/Testimonials'
+import Contact from './Components/Contact/Contact'
+import Footer from './Components/Footer/Footer'
+import VideoPlayer from './Components/VideoPlayer/VideoPlayer'
 
 const App = () => {
   const [playVideo, setPlayVideo] = useState(false);
@@ -18,21 +18,22 @@ const App = () => {
       <Navbar />
       <Hero />
       <div className="container">
-        <Title subTitle="What We Offer" title="What We Offer" />
+        <Title subTitle='What We Offer' title='What We Offer' />
         <Programs />
         <About setPlayVideo={setPlayVideo} />
-        <Title subTitle="Gallery" title="Campus Photos" />
+        <Title subTitle='Gallery' title='Campus Photos' />
         <Campus />
-        <Title subTitle="TESTIMONIALS" title="What Students Say" />
+        <Title subTitle='TESTIMONIALS' title='What Students Says' />
         <Testimonials />
-        <Title subTitle="Contact Us" title="Get in Touch" />
+        <Title subTitle='Contact Us' title='Get in Touch' />
         <Contact />
         <Footer />
       </div>
-      {playVideo && <VideoPlayer setPlayVideo={setPlayVideo} />}
+      <VideoPlayer playVideo={playVideo} setPlayVideo={setPlayVideo} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+
 
