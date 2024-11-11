2329194a1d12439e64321b954a73709f1b019a04
heyAnkit18 <ankitkumarjune18@gmail.com>
2024-11-11 18:16:30 +0530

    Footer.jsx Added

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,13 +4,17 @@ import './Footer.css'
 const Footer = () => {
   return (
     <div className='footer'>
-        <p>© 2024 All rights reserved.</p>
-        <ul>
-            <li>Terms of Services</li>
-            <li>Privacy Policy</li>
-        </ul>
+      <p>© 2024 All rights reserved. Your Company Name.</p>
+      <p>Creating a better future through technology.</p>
+      <p>Follow us on:</p>
+      <div className='social-icons'>
+        <a href='https://www.facebook.com' target='_blank' rel='noopener noreferrer'>Facebook</a> | 
+        <a href='https://www.twitter.com' target='_blank' rel='noopener noreferrer'>Twitter</a> | 
+        <a href='https://www.instagram.com' target='_blank' rel='noopener noreferrer'>Instagram</a>
+      </div>
     </div>
   )
 }
 
 export default Footer;
+
